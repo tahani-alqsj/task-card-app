80845b579a986976594e55c05286f11712e07e49
Tahani-alqadiry <tahani-alqsj@hotmail.com>
2018-06-03 01:52:48 +0300

    changes in files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
