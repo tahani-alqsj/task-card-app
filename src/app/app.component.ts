de1cea491174b58f09815ab8db0a830a31136f24
Tahani-alqadiry <tahani-alqsj@hotmail.com>
2018-06-06 01:58:10 +0300

    finshes the course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,24 @@
 import { Component } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import * as $ from 'jquery';
 import {Task} from './model/task'
 @Component({
-  moduleId:module.id,
+  // moduleId:module.id,
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
-  private tasks = [
-    new Task(
-      "buy a monkey",
-      false
-    ),
-
-    new Task(
-      "walk a turtule",
-      false
-    )
-  ]
-  private currentTask = new Task(null, false);
+  private tasks: Task[] = [];
+  private currentTask = new Task(null, false, true);
 
   addTask(){
-    let task = new Task(this.currentTask.content, this.currentTask.completed);
+    let task = new Task(this.currentTask.content, this.currentTask.completed, this.currentTask.status);
     this.tasks.push(task);
+    this.currentTask.content = null;
   }
+
+ 
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -2,14 +2,22 @@ import { Component, Input } from "@angular/core";
 import {Task} from '../model/task';
 
 @Component({
-    moduleId:module.id,
+    // moduleId:module.id,
     selector:'app-card',
-    templateUrl:'card.component.html',
-    styleUrls:['card.component.css']
+    templateUrl:'./card.component.html',
+    styleUrls:['./card.component.css']
 
 
 })
 
 export class CardComponent{
     @Input() task: Task;
+
+    statusToggle(){
+        this.task.completed = !this.task.completed;
+    }
+
+    delStatusToggle(){
+        this.task.status = !this.task.status;
+    }
 }
